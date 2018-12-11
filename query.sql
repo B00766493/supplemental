8c43bc7024c7de9977f5d7a366dc485d63961971
MD <mduane2030@gmail.com>
2018-12-11 09:29:36 +0000

    Update

---
+++ query.sql
@@ -0,0 +1,16 @@
+create table CustomerTable (
+	Destination VARCHAR(50),
+	Capacity VARCHAR(50),
+	Features VARCHAR(50),
+	Accessible VARCHAR(50)
+);
+insert into CustomerTable (Destination, Capacity, Features, Accessible) values 
+('Leitrim', '75', 'Cocktail Bar', 'It is accessible');
+insert into CustomerTable (Destination, Capacity, Features, Accessible) values 
+('Navan', '25', 'Mosh Pit', 'It is not accessible');
+insert into CustomerTable (Destination, Capacity, Features, Accessible) values 
+('Cahirsiveen', '65', 'Casino', 'It is accessible');
+insert into CustomerTable (Destination, Capacity, Features, Accessible) values 
+('Gweedore', '80', 'Tight-Rope', 'It is not accessible');
+insert into CustomerTable (Destination, Capacity, Features, Accessible) values 
+('Bangor', '50', 'Mud Wrestling', 'It is accessible');
